0741abcd9b56a131703b8d6bd9fd095e8db67d88
Wesley Aptekar-Cassels <me@wesleyac.com>
2020-07-07 22:26:25 -0400

    autonumber sidenotes

--- bin/process-markdown.sh
+++ bin/process-markdown.sh
@@ -1,18 +1,20 @@
 #!/usr/bin/env bash
 
+SIDENOTE_NUM=1
+
 while read -r line
 do
 	case "$line" in 
-		@sidenote:*:*)
-			SIDENOTE_ID=$(echo "$line" | cut -d: -f 2)
-			SIDENOTE_TEXT=$(echo "$line" | cut -d: -f 3)
+		@sidenote:*)
+			SIDENOTE_TEXT=$(echo "$line" | cut -d: -f 2)
 			cat <<SIDENOTE_HTML
-<label for="sn-$SIDENOTE_ID" class="margin-toggle sidenote-number"></label>
-<input type="checkbox" id="sn-$SIDENOTE_ID" class="margin-toggle"/>
-<span class="sidenote" id="sn-$SIDENOTE_ID">
+<label for="sn-$SIDENOTE_NUM" class="margin-toggle sidenote-number"></label>
+<input type="checkbox" id="sn-$SIDENOTE_NUM" class="margin-toggle"/>
+<span class="sidenote" id="sn-$SIDENOTE_NUM">
 $SIDENOTE_TEXT
 </span>
 SIDENOTE_HTML
+			SIDENOTE_NUM=$((SIDENOTE_NUM+1))
 		;;
 		*)
 			echo "$line"
