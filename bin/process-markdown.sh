0cb8d35d03748ac257242c56b5b564c9ffa85404
Wesley Aptekar-Cassels <me@wesleyac.com>
2020-07-08 00:20:22 -0400

    fix bug in sidenotes with colons

--- bin/process-markdown.sh
+++ bin/process-markdown.sh
@@ -6,7 +6,7 @@ while read -r line
 do
 	case "$line" in 
 		@sidenote:*)
-			SIDENOTE_TEXT=$(echo "$line" | cut -d: -f 2)
+			SIDENOTE_TEXT=$(echo "$line" | cut -d: -f 2-)
 			cat <<SIDENOTE_HTML
 <label for="sn-$SIDENOTE_NUM" class="margin-toggle sidenote-number"></label>
 <input type="checkbox" id="sn-$SIDENOTE_NUM" class="margin-toggle"/>
