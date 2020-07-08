3a2c4298f1f5b7bd7700c163cd0357c06c73ad62
Wesley Aptekar-Cassels <me@wesleyac.com>
2020-07-07 21:43:30 -0400

    add deploy script

---
+++ bin/deploy.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+set -e
+
+if ! [ -z "$(git status --porcelain)" ]; then 
+	echo "working directory not clean, exiting." 
+	exit 1
+fi
+
+if ! git diff --exit-code > /dev/null; then
+	echo "working directory not clean, exiting." 
+	exit 1
+fi
+
+if [ `git branch | grep gh-pages` ]
+then
+	git branch -D gh-pages
+fi
+git checkout -b gh-pages
+
+./bin/assemble.sh
+
+find . ! -name 'out' -type f -exec rm -f {} +
+mv out/* .
+rmdir out/
+
+git add -A
+git commit --allow-empty -m "$(git log -1 --pretty=%B)"
+git push -f -q origin gh-pages
+
+git checkout -
+
+echo "deployed <3"
+
+exit 0
