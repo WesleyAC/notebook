9d419d5b8417a2a9173826fd5260cf0c29ee671e
Wesley Aptekar-Cassels <me@wesleyac.com>
2020-07-09 06:01:20 -0400

    add post on tool heritage

--- bin/process-markdown.sh
+++ bin/process-markdown.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 SIDENOTE_NUM=1
+MARGINNOTE_NUM=1
 
 while read -r line
 do
@@ -10,11 +11,32 @@ do
 			cat <<SIDENOTE_HTML
 <label for="sn-$SIDENOTE_NUM" class="margin-toggle sidenote-number"></label>
 <input type="checkbox" id="sn-$SIDENOTE_NUM" class="margin-toggle"/>
-<span class="sidenote" id="sn-$SIDENOTE_NUM">
+<span class="sidenote">
 $SIDENOTE_TEXT
 </span>
 SIDENOTE_HTML
 			SIDENOTE_NUM=$((SIDENOTE_NUM+1))
+		;;
+		@marginnote:*)
+			MARGINNOTE_TEXT=$(echo "$line" | cut -d: -f 2-)
+			cat <<MARGINNOTE_HTML
+<label for="mn-$MARGINNOTE_NUM" class="margin-toggle">&#8853;</label>
+<input type="checkbox" id="mn-$MARGINNOTE_NUM" class="margin-toggle"/>
+<span class="marginnote">
+$MARGINNOTE_TEXT
+</span>
+MARGINNOTE_HTML
+			MARGINNOTE_NUM=$((SIDENOTE_NUM+1))
+		;;
+		@marginnote-mobilehide:*)
+			MARGINNOTE_TEXT=$(echo "$line" | cut -d: -f 2-)
+			cat <<MARGINNOTE_HTML
+<input type="checkbox" id="mn-$MARGINNOTE_NUM" class="margin-toggle"/>
+<span class="marginnote">
+$MARGINNOTE_TEXT
+</span>
+MARGINNOTE_HTML
+			MARGINNOTE_NUM=$((SIDENOTE_NUM+1))
 		;;
 		*)
 			echo "$line"
