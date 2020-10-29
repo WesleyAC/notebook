65e60bceced5f5fe5f1c61d0ab71afeb34b92da9
Wesley Aptekar-Cassels <me@wesleyac.com>
2020-10-29 11:12:56 -0700

    make sidenote lines prettier

--- parts/sideline.js
+++ parts/sideline.js
@@ -28,26 +28,35 @@ function setup_sideline() {
 		svg_line1.setAttribute("stroke", "#666");
 		svg_line1.setAttribute("x1", label_offset.left);
 		svg_line1.setAttribute("y1", label_offset.top + label_offset.height + 1);
-		svg_line1.setAttribute("x2", note_offset.left - 3);
+		svg_line1.setAttribute("x2", note_offset.left);
 		svg_line1.setAttribute("y2", label_offset.top + label_offset.height + 1);
 
 		let svg_line2 = document.createElementNS("http://www.w3.org/2000/svg", "line");
 		svg_container.appendChild(svg_line2);
 		svg_line2.setAttribute("stroke-width", "2px");
 		svg_line2.setAttribute("stroke", "#666");
-		svg_line2.setAttribute("x1", note_offset.left - 3);
-		svg_line2.setAttribute("y1", label_offset.top + label_offset.height + 1);
-		svg_line2.setAttribute("x2", note_offset.left - 3);
-		svg_line2.setAttribute("y2", note_offset.top);
+		svg_line2.setAttribute("x1", note_offset.left - 1);
+		svg_line2.setAttribute("y1", Math.min(label_offset.top + label_offset.height + 1, note_offset.top - 6));
+		svg_line2.setAttribute("x2", note_offset.left - 1);
+		svg_line2.setAttribute("y2", note_offset.top + note_offset.height + 6);
 
 		let svg_line3 = document.createElementNS("http://www.w3.org/2000/svg", "line");
 		svg_container.appendChild(svg_line3);
-		svg_line3.setAttribute("stroke-width", "4px");
+		svg_line3.setAttribute("stroke-width", "2px");
 		svg_line3.setAttribute("stroke", "#666");
-		svg_line3.setAttribute("x1", note_offset.left - 2);
-		svg_line3.setAttribute("y1", note_offset.top);
-		svg_line3.setAttribute("x2", note_offset.left - 2);
-		svg_line3.setAttribute("y2", note_offset.top + note_offset.height);
+		svg_line3.setAttribute("x1", note_offset.left - 1);
+		svg_line3.setAttribute("y1", note_offset.top - 6);
+		svg_line3.setAttribute("x2", note_offset.left + 18);
+		svg_line3.setAttribute("y2", note_offset.top - 6);
+
+		let svg_line4 = document.createElementNS("http://www.w3.org/2000/svg", "line");
+		svg_container.appendChild(svg_line4);
+		svg_line4.setAttribute("stroke-width", "2px");
+		svg_line4.setAttribute("stroke", "#666");
+		svg_line4.setAttribute("x1", note_offset.left - 1);
+		svg_line4.setAttribute("y1", note_offset.top + note_offset.height + 6);
+		svg_line4.setAttribute("x2", note_offset.left + 18);
+		svg_line4.setAttribute("y2", note_offset.top + note_offset.height + 6);
 
 		note.parentNode.insertBefore(svg_container, note.nextSibling); // might break if sidenote is last element in parent?
 	}
