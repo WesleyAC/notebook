6e40326fecd8b3584e31746a0b30364cba1e9274
Wesley Aptekar-Cassels <me@wesleyac.com>
2020-11-12 21:54:48 -0800

    make 404 page more fun

--- bin/build/assemble-404.sh
+++ bin/build/assemble-404.sh
@@ -7,9 +7,10 @@ source ./bin/build/vars.sh
 mkdir -p "$(dirname "$1")"
 
 sed \
-	-e "s/★PAGE_TITLE★/404 Error ⁑ $BLOG_NAME/g" \
+	-e "s/★PAGE_TITLE★/This entry does not exist ⁑ $BLOG_NAME/g" \
 	-e "s/★OG_TITLE★/404 Error/g" \
 	-e "s/★OG_TYPE★/website/g" \
+	-e "s/<script defer src=\"\/linktext.min.js\"><\/script>//g" \
 	-e "/★PAGE_CONTENT★/{
 		s/★PAGE_CONTENT★//g
 		r /dev/stdin
