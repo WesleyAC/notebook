0e181324edb94fc93f92c193714ca7280238b9f6
Wesley Aptekar-Cassels <me@wesleyac.com>
2021-07-12 19:21:03 -0700

    Add --yolo flag to deploy.sh.

--- bin/build/fix-sidenote-spacing.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-sed \
-	-e "s/[ ]*\(<label for='[^']*' class='margin-toggle sidenote-number'\)/\1/g" \
-	-e "s/<\/span> ,/<\/span>,/g"
--- bin/build/rewrite-time-format.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# shellcheck disable=SC2016
-xidel --silent --html - --xquery 'transform(/, function($e) { 
-	$e / (if (name() = "time") then 
-	<time datetime="{head(tokenize(@datetime, " "))}T{head(tail(tokenize(@datetime, " ")))}{head(tail(tail(tokenize(@datetime, " "))))}">{@* except @datetime, node()}</time>
-         else .)
-})'
