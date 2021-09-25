996b503a3c053bcdc30b12415c9d8eb7b8c9cf3e
Wesley Aptekar-Cassels <me@wesleyac.com>
2021-09-25 19:09:14 -0400

    Improve caching for atom.xml.

--- bin/build/build.py
+++ bin/build/build.py
@@ -112,7 +112,8 @@ try:
             build_ninja.write(f"build out/site/{out_file}/index.html: copy-file out/tmp/{out_file}/index.min.html\n")
             build_ninja.write(f"build out/site/{out_file}/{out_file}.md: copy-file entries/{entry}\n")
 
-        build_ninja.write(f"build out/tmp/index.html out/site/atom.xml: assemble-index-atom entries/{' entries/'.join(entry_files)} | bin/build/assemble-index-atom.sh bin/build/vars.sh parts/template.html parts/index.html parts/atom.xml\n")
+        build_ninja.write(f"build out/tmp/index.html out/tmp/atom.xml: assemble-index-atom entries/{' entries/'.join(entry_files)} | bin/build/assemble-index-atom.sh bin/build/vars.sh parts/template.html parts/index.html parts/atom.xml\n")
+        build_ninja.write("build out/site/atom.xml: copy-file out/tmp/atom.xml\n")
         build_ninja.write("build out/tmp/index.min.html: minify-html out/tmp/index.html\n")
         build_ninja.write("build out/site/index.html: copy-file out/tmp/index.min.html\n")
 
