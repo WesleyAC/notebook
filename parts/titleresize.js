63b61ab5740078e45e32136461be9f6a1e908720
Wesley Aptekar-Cassels <me@wesleyac.com>
2021-10-18 11:39:01 -0400

    Enforce minimum title font size.

--- parts/titleresize.js
+++ parts/titleresize.js
@@ -1,8 +1,9 @@
 function tr() {
+  let minFontSize = parseFloat(window.getComputedStyle(document.getElementById("dateline"))["font-size"].split("px")[0]);
 	for (let e of document.getElementsByTagName("h1")) {
 		if (e.scrollWidth != e.clientWidth) {
 			const fontSize = parseFloat(window.getComputedStyle(e)["font-size"].split("px")[0]);
-			e.style.fontSize = (fontSize * e.clientWidth / e.scrollWidth) + 'px';
+			e.style.fontSize = Math.max(fontSize * e.clientWidth / e.scrollWidth, minFontSize) + 'px';
 		}
 	}
 }
